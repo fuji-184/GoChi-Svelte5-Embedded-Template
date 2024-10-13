ea0229d3441a8f8d677d2e78a70e7f96d10ae0bc
Fuji <fujisantoso134@gmail.com>
2024-10-13 07:02:52 +0700

    updated

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module GoChi-Svelte5PreviewSSG-Embedded-Template
 
 go 1.22.2
 
-require github.com/go-chi/chi/v5 v5.1.0 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+)
--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+    "database/sql"
     "embed"
+    "encoding/json"
     "fmt"
     "io/fs"
     "log"
     "net/http"
+    "time"
+
     "github.com/go-chi/chi/v5"
+    _ "github.com/mattn/go-sqlite3"
 )
 
 //go:embed all:svelte/build
 var svelteStatic embed.FS
 
-func main() {
+type Tes struct {
+    Id   int    `json:"id"`
+    Name string `json:"name"`
+}
 
+func main() {
     s, err := fs.Sub(svelteStatic, "svelte/build")
     if err != nil {
         panic(err)
@@ -21,17 +30,73 @@ func main() {
 
     staticServer := http.FileServer(http.FS(s))
 
-    r := chi.NewRouter()
+    db, err := sql.Open("sqlite3", "./fuji.db")
+    if err != nil {
+        log.Fatal(err)
+    }
 
+    db.SetMaxOpenConns(10)
+    db.SetMaxIdleConns(5)
+    db.SetConnMaxLifetime(time.Hour)
+
+    defer db.Close()
+
+    r := chi.NewRouter()
     r.Handle("/", staticServer)
     r.Handle("/_app/*", staticServer)
-	r.Handle("/service-worker.js", staticServer)
+    r.Handle("/service-worker.js", staticServer)
     r.Handle("/favicon.png", staticServer)
+
+    r.Get("/tes", func(w http.ResponseWriter, r *http.Request) {
+        handleTes(w, r, db)
+    })
+
     r.HandleFunc("/*", func(w http.ResponseWriter, r *http.Request) {
         r.URL.Path = "/"
         staticServer.ServeHTTP(w, r)
     })
 
-    fmt.Println("Running on port: 3000")
-    log.Fatal(http.ListenAndServe(":3000", r))
+    fmt.Println("Running on port: 8000")
+    log.Fatal(http.ListenAndServe(":8000", r))
+}
+
+func handleTes(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+    _, err := db.Exec(`CREATE TABLE IF NOT EXISTS tes (id INTEGER NOT NULL PRIMARY KEY, name TEXT)`)
+    if err != nil {
+        http.Error(w, "Error creating table", http.StatusInternalServerError)
+        return
+    }
+
+    stmt, err := db.Prepare("INSERT INTO tes(name) VALUES(?)")
+    if err != nil {
+        http.Error(w, "Error preparing insert statement", http.StatusInternalServerError)
+        return
+    }
+    defer stmt.Close()
+
+    _, err = stmt.Exec("fuji")
+    if err != nil {
+        http.Error(w, "Error inserting data", http.StatusInternalServerError)
+        return
+    }
+
+    rows, err := db.Query("SELECT id, name FROM tes")
+    if err != nil {
+        http.Error(w, "Error querying data", http.StatusInternalServerError)
+        return
+    }
+    defer rows.Close()
+
+    var results []Tes
+    for rows.Next() {
+        var tes Tes
+        if err := rows.Scan(&tes.Id, &tes.Name); err != nil {
+            http.Error(w, "Error scanning row", http.StatusInternalServerError)
+            return
+        }
+        results = append(results, tes)
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(results)
 }
